83466c3cd35ad275d6477c3a0e4f80f8ea4f6626
JammerKelt <tejml.jn@gmail.com>
2018-09-29 13:49:48 +0000

    using jinja template system

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,6 +1,8 @@
 from flask import Flask
 from flask import render_template
 
+from .database import articles
+
 flask_app = Flask(__name__)
 
 @flask_app.route("/")
@@ -17,4 +19,11 @@ def view_admin():
 
 @flask_app.route("/articles/")
 def view_articles() :
-    return render_template("articles.html")
+    return render_template("articles.html", articles=articles.items())
+
+@flask_app.route("/articles/<int:art_id>")
+def view_article(art_id):
+    article = articles.get(art_id)
+    if article:
+        return render_template("article.html", article=article)
+    return render_template("article_not_found.html", art_id=art_id)
