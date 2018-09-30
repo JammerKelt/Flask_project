af1693d60d66ad42b2bb17db06f49970f9309717
JammerKelt <tejml.jn@gmail.com>
2018-09-30 05:34:07 +0000

    login form + session management

--- mdblog/app.py
+++ mdblog/app.py
@@ -1,9 +1,14 @@
 from flask import Flask
 from flask import render_template
+from flask import request
+from flask import redirect
+from flask import url_for
+from flask import session
 
 from .database import articles
 
 flask_app = Flask(__name__)
+flask_app.secret_key = b'g\xec\x11VG\x8dj\x14B\xfb\xf7m \xbe\xc4\x10u\x18\xb8\n6o{w'
 
 @flask_app.route("/")
 def view_welcome_page():
@@ -15,6 +20,8 @@ def view_about():
 
 @flask_app.route("/admin/")
 def view_admin():
+    if "logged" not in session:
+        return redirect(url_for("view_login"))
     return render_template("admin.html")
 
 @flask_app.route("/articles/")
@@ -27,3 +34,22 @@ def view_article(art_id):
     if article:
         return render_template("article.html", article=article)
     return render_template("article_not_found.html", art_id=art_id)
+
+@flask_app.route("/login/", methods=["GET"])
+def view_login():
+    return render_template("login.html")
+
+@flask_app.route("/login/", methods=["POST"])
+def login_user():
+    username = request.form["username"]
+    password = request.form["password"]
+    if username == "admin" and password == "admin":
+        session["logged"] = True
+        return redirect(url_for("view_admin"))
+    else:
+        return redirect(url_for("view_login"))
+
+@flask_app.route("/logout/", methods=["POST"])
+def logout_user():
+    session.pop("logged")
+    return redirect(url_for("view_welcome_page"))
